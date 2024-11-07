27c9e6e6d8793dea806db0124b3658166768eb88
Asaduzzaman Kishan <asaduzzamankishan2@gmail.com>
2024-11-07 12:15:30 +0600

    Single Number

--- week01/leetcode136.cpp
+++ week01/leetcode136.cpp
@@ -1,15 +1,14 @@
-// 136. Single Number
-// https://leetcode.com/problems/single-number/description/
+// 136. Single Number >> https://leetcode.com/problems/single-number/
+// Time: O(N) where N is the size of the array
+// Space: O(1)
 
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int res = 0;
-
         for(int num : nums) {
             res ^= num;
         }
-
         return res;
     }
 };
