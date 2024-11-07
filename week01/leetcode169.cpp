e105d993aafcb44b9060c0d4eecb0b7440c4e56c
Asaduzzaman Kishan <asaduzzamankishan2@gmail.com>
2024-11-07 14:26:22 +0600

    Majority Element

--- week01/leetcode169.cpp
+++ week01/leetcode169.cpp
@@ -1,20 +1,18 @@
-// 169. Majority Element
-// https://leetcode.com/problems/majority-element/description/
+// 169. Majority Element >> https://leetcode.com/problems/majority-element/
+// Time: O(N) where N is the size of the array
+// Space: O(1)
 
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int maj_ele = 0;
+        int maj_ele;
         int count = 0;
-
         for(int num : nums) {
             if(!count) {
                 maj_ele = num;
             }
-
             (num == maj_ele) ? count++ : count--;
         }
-
         return maj_ele;
     }
 };
