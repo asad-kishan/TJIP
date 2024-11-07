0e93297bb3f4abedcdacd835f3fab8bd25619db5
Asaduzzaman Kishan <asaduzzamankishan2@gmail.com>
2024-11-07 15:57:45 +0600

    Move Zeroes

--- week01/leetcode283.cpp
+++ week01/leetcode283.cpp
@@ -1,12 +1,11 @@
-// 283. Move Zeroes
-// https://leetcode.com/problems/move-zeroes/description/
+// 283. Move Zeroes >> https://leetcode.com/problems/move-zeroes/
+// Time: O(N) where N is the size of the array
+// Space: O(1) 
 
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int l = 0;
-        int r = 1;
-
+        int l = 0, r = 1;
         while(r < nums.size()) {
             if(nums[l]) l++; 
             else {
