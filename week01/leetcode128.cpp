0735e3cd77f9297eaa250963d0cd73b239d9494c
Asaduzzaman Kishan <asaduzzamankishan2@gmail.com>
2024-11-07 12:09:28 +0600

    Longest Consecutive Sequence

--- week01/leetcode128.cpp
+++ week01/leetcode128.cpp
@@ -1,19 +1,16 @@
-// 128. Longest Consecutive Sequence
-// https://leetcode.com/problems/longest-consecutive-sequence/description/
+// 128. Longest Consecutive Sequence >> https://leetcode.com/problems/longest-consecutive-sequence/
+// Time: O(N) where N is the size of the array
+// Space: O(N) where N is the size of the array
 
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.empty()) return 0;
-
         unordered_set<int> numSet(nums.begin(), nums.end());
         int res = 0;
         for(int num : nums) {
-            if(numSet.count(num - 1)) {
-                continue;
-            } else {
-                int count = 0;
-                int curr = num;
+            if(numSet.count(num - 1) == 0) {
+                int count = 1;
+                int curr = num+1;
                 while(numSet.count(curr)) {
                     numSet.erase(curr);
                     curr++;
