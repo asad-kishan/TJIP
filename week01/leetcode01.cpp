8a382a6b28cd3251bed4bfb34c802f8c2fce675e
Asaduzzaman Kishan <asaduzzamankishan2@gmail.com>
2024-11-07 11:26:09 +0600

    Two Sum

--- week01/leetcode01.cpp
+++ week01/leetcode01.cpp
@@ -1,24 +1,19 @@
-// 1. Two Sum
-// https://leetcode.com/problems/two-sum/
+// 1. Two Sum >> https://leetcode.com/problems/two-sum/
+// Time: O(N) where N is the size of the array
+// Space: O(N) where N is the size of the array
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> hashTable;
-        vector<int> result;
-
         for(int i = 0; i < nums.size(); i++) {
             int expected_val = target - nums[i];
-            
             if(hashTable.find(expected_val) == hashTable.end()) {
                 hashTable[nums[i]] = i;
             } else {
-                result.push_back(hashTable[expected_val]);
-                result.push_back(i);
-                break;
+                return {hashTable[expected_val], i};
             }
         }
-
-        return result;
+        return {};
     }
 };
